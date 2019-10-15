b30eac687253f20e5d917960f3bcb2ef1847c32d
GUL-Bao <BaoPham92@yahoo.com>
2019-10-15 11:53:19 -0400

    Setup

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react'; 
+
+import DashBoard from './DashBoard';
+
+test('DashBoard renders correctly', () => {
+    render(<DashBoard />);
+}); 
